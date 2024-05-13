cd95e63ad19e4a7b98562c3c349bd953bfdbc233
Fa1tumn <ts01088978343@gmail.com>
2024-05-13 16:30:02 +0900

    add model

--- Matrix.py
+++ Matrix.py
@@ -6,3 +6,14 @@ class Matrix:
         self.rows=rows
         self.cols=cols
         self.data=[[0]*cols for _ in range(rows)]
+    def __add__(self, other):
+        if self.rows != other.rows or self.cols != other.cols:
+            raise SizeMismatchException("Matrix size mismatch")
+
+        result = Matrix(self.rows, self.cols)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                result.data[i][j] = self.data[i][j] + other.data[i][j]
+        return result
+    def __repr__(self):
+        return '\n'.join([' '.join(map(str, row)) for row in self.data])
