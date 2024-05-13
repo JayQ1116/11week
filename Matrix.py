87e5e786c505403c60b1d8c75f6ba1645a811a0a
Fa1tumn <ts01088978343@gmail.com>
2024-05-13 17:08:48 +0900

    2

--- Matrix.py
+++ Matrix.py
@@ -2,15 +2,15 @@ class SizeMismatchException(Exception):
     pass
 
 class Matrix:
-    def __init__(self,rows,cols):
-        self.rows=rows
-        self.cols=cols
-        self.data=[[0]*cols for _ in range(rows)]
+    def __init__(self,data):
+        self.data=data
+        self.rows=len(data)
+        self.cols=len(data[0])
     def __add__(self, other):
         if self.rows != other.rows or self.cols != other.cols:
             raise SizeMismatchException("Matrix size mismatch")
 
-        result = Matrix(self.rows, self.cols)
+        result = Matrix([[0]*self.cols for _ in range(self.rows)])
         for i in range(self.rows):
             for j in range(self.cols):
                 result.data[i][j] = self.data[i][j] + other.data[i][j]
@@ -21,8 +21,7 @@ class Matrix:
     def __mul__(self, other):
         if self.cols != other.rows:
             raise SizeMismatchException("Matrix size mismatch")
-
-        result = Matrix(self.rows, other.cols)
+        result = Matrix([[0]*other.cols for _ in range(self.rows)])
         for i in range(self.rows):
             for j in range(other.cols):
                 for k in range(self.cols):
@@ -31,21 +30,11 @@ class Matrix:
         return result
 
 if __name__ == "__main__":
-    A = Matrix(2, 2)
-    A.data = [[1, 2],
-              [3, 4],]
+    A = Matrix([[1, 2],[3, 4],])
+    B = Matrix([[3,4],[5,6]])
 
-    B = Matrix(2, 2)
-    B.data = [[3,4],
-              [5,6]]
-
-    C = Matrix(3,2)
-    C.data = [[3,4],
-              [5,6],
-              [1,2]]
-    D = Matrix(2,3)
-    D.data = [[3,4,5],
-              [5,6,7]]
+    C = Matrix([[3,4],[5,6],[1,2]])
+    D = Matrix([[3,4,5],[5,6,7]])
     print(A)
     print(A+B)
     print(A*D)
