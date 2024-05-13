101b4030b23ebe2e51f285ea3a28227308e530a5
Fa1tumn <ts01088978343@gmail.com>
2024-05-13 16:28:17 +0900

    1

--- Matrix.py
+++ Matrix.py
@@ -2,7 +2,7 @@ class SizeMismatchException(Exception):
     pass
 
 class Matrix:
-    def __init__(self,rows,cols,data):
+    def __init__(self,rows,cols):
         self.rows=rows
         self.cols=cols
         self.data=[[0]*cols for _ in range(rows)]
